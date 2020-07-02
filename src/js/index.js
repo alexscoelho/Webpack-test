8af995e6ff9c2e6be4252764ec56da17a6335bf8
alexscoelho <alexsoncoelho21@hotmail.com>
2020-07-02 14:41:12 +0000

    first commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,29 @@
 /* eslint-disable */
-
-import "../assets/img/rigo-baby.jpg";
-import "../assets/img/4geeks.ico";
 //import 'breathecode-dom'; //DOM override to make JS easier to use
 import "../style/index.scss";
 
-window.onload = function() {
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#the-excuse").innerHTML = generateExcuse();
+  });
+
+  console.log("hello Alex");
+};
+
+let generateExcuse = () => {
+  let who = ["my car", "my dog", "my cel", "the mountains", "a ship"];
+  let what = ["ate", "washed", "clean", "delete", "turn off"];
+  let where = [
+    "in Alaska",
+    "my castle",
+    "the bedroom",
+    "the street",
+    "the sea"
+  ];
+
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let whatIndex = Math.floor(Math.random() * what.length);
+  let whereIndex = Math.floor(Math.random() * where.length);
+
+  return who[whoIndex] + " " + what[whatIndex] + " " + where[whereIndex];
 };
